9182bdf2878f1eae9927dc211287412cd841669d
Freewind <nowindlee@gmail.com>
2024-02-24 15:08:21 +0800

    typescript-typing-merge-multiple-types-demo

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-TypeScript Hello World Demo
+TypeScript Typing Merge Multiple Types Demo
 ===========================
 
 ```
 npm install
 npm start
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,31 @@
-console.log('Hello');
+import { Object } from 'ts-toolbelt';
+
+type AAA = {
+    type: 'aaa',
+    aaa: string;
+}
+
+type BBB = {
+    type: 'bbb',
+    bbb: string
+}
+
+type Union = AAA | BBB
+
+type New = Object.Merge<AAA, BBB>
+
+const x: New = {
+    type: 'aaa',
+    aaa: '111',
+    bbb: '222'
+}
+
+console.log(x);
+
+
+
+
+
+
+
+
